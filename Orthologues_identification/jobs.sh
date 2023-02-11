77812300c248fed91f72f07f2352a0fcd03178fd
Anjana_CK <anjana.karawita@csiro.au>
2023-02-12 08:02:51 +1000

    Update jobs.sh

--- Orthologues_identification/jobs.sh
+++ Orthologues_identification/jobs.sh
@@ -10,4 +10,5 @@ cat orthologs.m8 | awk '$3 < 1e-05 {print $1"\t"$2}' > orthologs.tsv
 python deduplicate.py orthologs.tsv final_orthologs.tsv
 python deduplicate2.py final_orthologs.tsv orthololist_human_vs_bat.tsv
 
-python replace_values.py ../RNAseq_pipeline/palecto/htseq/ orthololist_human_vs_bat.tsv
+python replace_values_bat.py ../RNAseq_pipeline/palecto/htseq/ orthololist_human_vs_bat.tsv
+python replace_values_human.py ../RNAseq_pipeline/human/htseq/ ../RNAseq_pipeline/palecto/htseq/T5ortho.tsv
