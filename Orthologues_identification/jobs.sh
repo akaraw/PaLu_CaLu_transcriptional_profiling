f7a319c21e76dd6cb5b37f265691af61b8fdab10
Anjana_CK <anjana.karawita@csiro.au>
2023-02-11 21:10:36 +1000

    Update jobs.sh

--- Orthologues_identification/jobs.sh
+++ Orthologues_identification/jobs.sh
@@ -7,6 +7,7 @@ mmseqs createdb human_long.faa mandb
 mmseqs search batdb mandb bat_man_hits tmp --max-seqs 1000 --min-seq-id 0.5
 mmseqs convertalis  batdb mandb bat_man_hits orthologs.m8 --format-output "query,target,evalue,pident"
 cat orthologs.m8 | awk '$3 < 1e-05 {print $1"\t"$2}' > orthologs.tsv
-python deduplicate.py orthologs.tsv > final_orthologs.tsv
+python deduplicate.py orthologs.tsv final_orthologs.tsv
+python deduplicate2.py final_orthologs.tsv orthololist_human_vs_bat.tsv
 
 
